f75a49126127126a89e5e7931f1b02856eac44ff
heineiuo <heineiuo@gmail.com>
2019-12-25 22:40:41 +0800

    v0.3.8

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,49 @@
 // rollup.config.js
-// import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
-import minify from 'rollup-plugin-babel-minify'
-import generatePackageJson from 'rollup-plugin-generate-package-json'
-import typescript from 'rollup-plugin-typescript'
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import babel from "rollup-plugin-babel";
+import minify from "rollup-plugin-babel-minify";
+import generatePackageJson from "rollup-plugin-generate-package-json";
+import builtins from "builtin-modules";
+import pkg from "./package.json";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    file: 'build/release/index.js',
-    format: 'cjs',
+    file: "build/release/index.js",
+    format: "cjs"
   },
+  external: [builtins, ...Object.keys(pkg.dependencies)],
   plugins: [
-    // resolve({
-    //   preferBuiltins: true
-    // }),
-    typescript(),
+    resolve({
+      extensions,
+      preferBuiltins: true
+    }),
     commonjs({
-      include: 'node_modules/**',
+      include: "node_modules/**",
       ignoreGlobal: false,
       sourceMap: false,
-      namedExports: {},
+      namedExports: {}
     }),
     json({
-      include: 'node_modules/**',
+      include: "node_modules/**",
       preferConst: true,
-      indent: '  ',
+      indent: "  ",
       compact: true,
-      namedExports: true,
+      namedExports: true
     }),
     babel({
-      exclude: 'node_modules/**',
+      extensions,
+      include: ["src/**/*"],
+      exclude: "node_modules/**"
     }),
     minify({
       comments: false,
-      sourceMap: false,
+      sourceMap: false
     }),
-    generatePackageJson(),
-  ],
-}
+    generatePackageJson()
+  ]
+};
